2ac78577ce084d8b070186fb5b924a6cfecade5d
LJZ <jz.long@qq.com>
2023-10-12 23:30:28 +0800

    refactor(clippy.rs): 尝试修复错误

--- exercises/clippy/clippy3.rs
+++ exercises/clippy/clippy3.rs
@@ -4,7 +4,9 @@
 #[allow(unused_variables, unused_assignments)]
 fn main() {
     let my_option: Option<()> = None;
-    if my_option.is_none() {}
+    if my_option.is_none() {
+        println!("What's in my_option is: {:#?}", my_option);
+    }
 
     let my_arr = &[-1, -2, -3 - 4, -5, -6];
     println!("My array! Here it is: {:?}", my_arr);
